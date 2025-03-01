5cab516477138fd905d40ef943677c43860fcda4
Manu01-alt <manuuuumod@gmail.com>
2025-03-01 10:06:25 +0100

    01/03/25

---
+++ CodeBlocks/Hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (void) {
+
+	puts("\nHello 3C!!");
+return 0;
+}
--- Python/Le Funzioni.py
+++ Python/Le Funzioni.py
@@ -4,3 +4,17 @@ def moltiplica_per_cinque(numero):
 numero = int(input("Inserisci un numero: ")) 
 
 print(moltiplica_per_cinque(numero))
+
+def my_function(food):
+    for x in food:
+        print(x)
+
+lista = ["A","B","C"]
+my_function(lista)
+
+def marco():
+    pass
+
+
+
+
