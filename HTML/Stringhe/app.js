93026139f1444de4f0cc5587574e8cb9e7882350
Manu01-alt <manuuuumod@gmail.com>
2025-10-20 10:07:48 +0200

    Fix input ID in stampa function and update HTML title for consistency

--- HTML/Stringhe/app.js
+++ HTML/Stringhe/app.js
@@ -1,10 +1,10 @@
 function stampa() {
-    let frase = document.getElementById("text").value;
+    let frase = document.getElementById("inputText").value;
     let char = frase.charAt(frase.length - 1);
 
-    if (char === "?") 
+    if (char == "?") 
         document.getElementById("Esercizio").innerText = "Non lo so..";    
-    else if (char === "!") 
+    else if (char == "!") 
         document.getElementById("Esercizio").innerText = "Hai ragione!";
     else 
         document.getElementById("Esercizio").innerText = "Riformula la frase.";
