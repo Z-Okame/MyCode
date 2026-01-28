628b1c52c773c46c90aba814240747c357d1c8f9
Manu01-alt <manuuuumod@gmail.com>
2026-01-28 16:09:55 +0100

    Mark subprojects as dirty for EthernetFrameII, Platformer, and Wordle1

--- Java/JavaFX/JavaFX_Tutorial/Calcolatrice.java
+++ Java/JavaFX/JavaFX_Tutorial/Calcolatrice.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -10,6 +11,7 @@ import javafx.scene.layout.BorderPane;
 public class Calcolatrice extends Application{
     
     private Button[] buttons;
+    private TextField risultato;
     private String[] strings = {
                                 "1", "2", "3", "X",
                                 "4", "5", "6", "+",
@@ -22,23 +24,41 @@ public class Calcolatrice extends Application{
         buttons = new Button[16];
 
         BorderPane root = new BorderPane();
+
         Button btnChiudi = new Button("Chiudi");
         root.setTop(btnChiudi);
+        btnChiudi.setOnAction(e -> stage.close());
+
         GridPane tastiera = new GridPane();
         root.setCenter(tastiera);
+        tastiera.setHgap(4);
+        tastiera.setVgap(4);
 
         for(int i = 0; i < 16; i++) {
             buttons[i] = new Button(strings[i]);
             int riga = i/4;
             int colonna = i%4;
+            buttons[i].setOnAction(e -> clickBottone(e));
             tastiera.add(buttons[i],colonna,riga);
         }
+
+        risultato = new TextField();
+        root.setBottom(risultato);
 
         root.setPadding(new Insets(15));
 
         stage.setScene(new Scene(root, 300, 200));
         stage.show();
 
+
     }
+
+    private void clickBottone(Event e) {
+        Button btn = (Button) e.getSource();
+        risultato.appendText(btn.getText());
+    }
+
+
+
     public static void main(String[] args) { launch(); }
 }
