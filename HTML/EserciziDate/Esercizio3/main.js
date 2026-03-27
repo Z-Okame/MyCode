12d63063b11b357604545ef5758ca85a3c35efb2
Manu <manuuuumod@gmail.com>
2026-03-27 12:54:55 +0100

    fixed bug in ES.3

--- HTML/EserciziDate/Esercizio3/main.js
+++ HTML/EserciziDate/Esercizio3/main.js
@@ -1,18 +1,18 @@
 let data = new Date(document.getElementById("data1").value);
 let n = parseInt(document.getElementById("N").value);
-
 data.setDate(parseInt(data.getDate() + n));
 
 function VisualizzaDataNuovaEFineMese() {
-
     let str = " La data tra " + String(n) + " giorni è: " + data.toDateString();
-
-    let fineMese = data.getMonth();
-
-    while (fineMese.getMonth() === data.getMonth()) {
+    
+    let fineMese = new Date(data); 
+    fineMese.setDate(fineMese.getDate() + 1); 
+    
+    while (fineMese.getMonth() === data.getMonth()) { 
         fineMese.setDate(fineMese.getDate() + 1);
     }
-
+    fineMese.setDate(fineMese.getDate() - 1); 
+    
     document.getElementById("calcoloN").innerText = str + " e il giorno di fine mese è: " + fineMese.toDateString();
 }
 
