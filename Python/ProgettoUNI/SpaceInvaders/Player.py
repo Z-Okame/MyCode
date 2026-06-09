63840de789ad7f733ed2a06697b2613a3a83ff4b
Manu <manuuuumod@gmail.com>
2026-06-09 15:48:55 +0200

    added new project SpaceInvaders

---
+++ Python/ProgettoUNI/SpaceInvaders/Player.py
@@ -0,0 +1,13 @@
+class Player:
+
+#dati sprite
+#(348, 364) (27, 15)
+    
+    def __init__(self, x0: int, y0: int):
+        self._x = x0
+        self._y = y0
+    
+    
+    def move():
+        # . . .
+        pass
